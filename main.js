aba16358b7031c26e5d007f56f3b0e75e5c510f8
flavio-secondini <flavio.secondini@gmail.com>
2021-04-13 16:54:43 +0200

    Interazione col server funzionante

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+Vue.config.devtools = true;
+
 var app = new Vue ({
   el:'#root',
   data: {
-    prova:'sa, sa, prova'
-},
-  methods: ''
+    prova: 'funziona',
+    albums: [],
+  },
+  mounted () {
+    let origine = this
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(function (response) {
+    origine.albums = response.data.response;
+    console.log(origine.albums);
+    });
+  },
+  methods: {
+
+  }
 })
